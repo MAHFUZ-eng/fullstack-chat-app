9b693b10054730e6f5934e2e1b9efa4f6f1ff8c7
Your Name <your@email.com>
2026-02-10 17:49:07 +0600

    fixes1

--- backend/src/index.js
+++ backend/src/index.js
@@ -38,7 +38,7 @@ app.use("/api/friends", friendRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
-  app.get("/:any(.*)", (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
   });
 }
